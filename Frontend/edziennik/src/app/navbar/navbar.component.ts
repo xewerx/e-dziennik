529285106bc476e3397c75d42bcf12bcfed3b10a
Ewaryst <ewaryst@spoko.pl>
2020-05-21 21:52:09 +0200

    rwd done, timer

--- Frontend/edziennik/src/app/navbar/navbar.component.ts
+++ Frontend/edziennik/src/app/navbar/navbar.component.ts
@@ -5,19 +5,29 @@ import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-
   constructor(private router: Router, public service: AuthService) { }
+   time = '';
 
   ngOnInit(): void {
-  }
+    this.timer();
 
-
+   }
   logoutUser() {
     localStorage.removeItem('token');
     localStorage.removeItem('login');
     this.router.navigate(['/login']);
   }
+
+  timer() {
+    setInterval(() => {
+    const today = new Date();
+    this.time = today.toLocaleDateString() + '  ' + today.toLocaleTimeString();
+    }, 1000);
+  }
+
+
+
 }
